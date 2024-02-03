94a9a2e30b5121e75750faa8dd36519ccc03c426
Tommaso Desiderio on WSL <desideriotommaso@gmail.com>
2024-02-03 22:09:22 +0100

    feat: added a medium line in the loss plot

--- data/visualize_loss.py
+++ data/visualize_loss.py
@@ -14,15 +14,20 @@ with open(file_path, 'r') as file:
 # Generate x-axis values (iteration numbers)
 iterations = list(range(1, len(packet_lost) + 1))
 
+# Calculate the medium value of packet loss
+medium_packet_lost = sum(packet_lost) / len(packet_lost)
+
 # Plot the data
 plt.figure(figsize=(10, 6))
-plt.plot(iterations, packet_lost, color='blue', marker='o', linestyle='-')
+plt.plot(iterations, packet_lost, color='blue', marker='o', linestyle='-', label='Packets Lost')
+plt.axhline(y=medium_packet_lost, color='red', linestyle='--', label='Medium Packet Lost')  # Add medium line
 plt.title('Number of Packets Lost Over Iterations')
 plt.xlabel('Iteration')
 plt.ylabel('Packets Lost')
 plt.grid(True)
 plt.tight_layout()
+plt.legend()  # Show legend
 
 # Save the image to a file
-plt.savefig("./data/loss_plot.png") 
+plt.savefig("./data/loss_plot.png")
 plt.close()
